5a33e0afc28f6a84c7760d9c6ad8a5e7fa79438e
fcomarcet1 <fcomarcet1@gmail.com>
2021-10-24 13:56:26 +0200

    Add resquest info middleware && node-info module

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
 ("use strict");
 
+// ********************* IMPORT MODULES *************************
 require("dotenv").config();
 const express = require("express");
 const connectDB = require('./db/connect');
+const nodeInfo = require('nodejs-info');
 
 const app = express();
 
+process.env.TZ="Europe/Madrid"
 
-// Request Logger Middleware
+
+// ******************* MIDDLEWARES ***************************
+
+
+
+// ********************* Request Logger Middleware *************************
 app.use(function (req, res, next) {
   // method path - ip
   let logger = 'Method: '+ req.method + ' | ' +'Path: '+ req.path + ' | ' + 'Ip: ' + req.ip;
   console.log(`Request--> ${logger}`);
   next();
-})
+});
 
-// test route
+// ********************* CORS *******************************
+
+
+// **************** LOAD FILE ROUTES ************************
+
+
+// ******************* REWRITE PATH ROUTES *******************
+
+
+// ********************* TEST ROUTES *************************
 app.get("/", (req, res) => {
   res.send(`<h1>Store-API</h1>
             <a href="#">Products link</a>`);
 });
 
+app.get("/node-info", (req, res) => {
+  res.send(nodeInfo(req));
+});
 
+
+// ********************* HTTP SERVER && DB *************************
 const port = process.env.APP_PORT;
 
 // DB && http server
@@ -37,3 +59,8 @@ const start = async () => {
 };
 
 start();
+
+
+
+// ********************* EXPORT APP *************************
+module.exports = app;
