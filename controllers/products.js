5f83f556f81ce82d6a56583b4c6fa3f856611d1e
fcomarcet1 <fcomarcet1@gmail.com>
2021-10-25 18:45:34 +0200

    Add paginate results and limit per page

--- controllers/products.js
+++ controllers/products.js
@@ -102,8 +102,15 @@ const getProductsStatic = async (req, res) => {
         const fieldsList = fields.split(',').join(' ');
         console.log(fieldsList);
         result = result.select(fieldsList);
-      }
+    }
 
+    //* Limit & page
+    // {{URL}}/products?sort=-name,&fields=name, price&limit=3&page=2
+    const limit = Number(req.query.limit) || 10;
+    const page = Number(req.query.page) || 1;
+    const skip = (page - 1) * limit;
+
+    result = result.skip(skip).limit(limit)
 
     const products = await result;
     //const products = await Product.find(queryObject);
